c90886373ff4c5878b5ac21d9ab2c3d2376d0fd8
Dona <a_uzakbaeva@bk.ru>
2022-03-02 14:21:09 +0300

    new changes

--- src/tab.js
+++ src/tab.js
@@ -50,10 +50,60 @@ $('.voyageka').slick({
 $('.voyageka1').slick({
 
   infinite: true,
-
+autoplay:true,
   slidesToShow: 1,
   slidesToScroll: 1,
 });
+
+
+
+
+
+const requestBtn = document.querySelector('#request-btn')
+
+const modal = document.querySelector('#custom-modal')
+
+const modalRequest = document.querySelector('#modal-request')
+const requestCloseBtn = document.querySelector('#request-close-btn')
+const submitBtn = document.querySelector('#modal-request-submit')
+
+const modalThanks = document.querySelector('#modal-thanks')
+const thanksCloseBtn = document.querySelector('#request-close-btn-thanks')
+
+
+requestBtn.addEventListener('click', () => {
+  addClass(modal)
+  addClass(modalRequest)
+})
+
+requestCloseBtn.addEventListener('click', () => {
+  removeClass(modal)
+})
+
+submitBtn.addEventListener('click', () => {
+  removeClass(modalRequest)
+  addClass(modalThanks)
+  setTimeout(() => {
+    removeClass(modal)
+    removeClass(modalThanks)
+  }, 5000)
+})
+
+thanksCloseBtn.addEventListener('click', () => {
+  removeClass(modalThanks)
+  removeClass(modal)
+  removeClass(modalRequest)
+})
+
+
+function removeClass(selector) {
+  selector.classList.remove('active')
+}
+
+function addClass(selector) {
+  selector.classList.add('active')
+}
+
 
 
 
